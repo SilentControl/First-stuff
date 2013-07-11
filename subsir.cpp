ff40c0a44ec8efb5529b48da19bdedec356e1742
SilentControl <robbfrostme@yahoo.com>
2013-07-11 21:20:57 +0300

    Maximum subsequence (DP)

---
+++ subsir.cpp
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+int max(int a, int b)
+{
+  return a > b ? a : b;
+}
+
+int LCS(int a[], int m, int b[], int n)
+{
+  if (m == -1 || n == -1)
+    return 0;
+  else
+    if (a[m] == a[n])
+      return 1 + LCS(a,m-1,b,n-1);
+    else
+      return max(LCS(a,m-1,b,n),LCS(a,m,b,n-1));
+}
+
+int main()
+{
+  int a[] = {1,2,3,22,23,4,5};
+  int m = 6; // int m = sizeof(a)/sizeof(a[0]) - 1;
+  int b[] = {1,3,2};
+  int n = 2; // int n = sizeof(b)/sizeof(b[0]) - 1;
+  printf("%d",LCS(a,m,b,n));
+  return 0;
+}
